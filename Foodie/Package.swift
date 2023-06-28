cddc86ecb0b2ddfa1ed6c2caf62edd9d38e379ee
Austin Rovge <austinrovge@gmail.com>
2023-06-27 21:59:58 -0500

    create xcodeproj and start using FoodieUI

--- Foodie/Package.swift
+++ Foodie/Package.swift
@@ -5,19 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "Foodie",
+    platforms: [
+      .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "FoodieUI",
+            targets: ["FoodieUI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
+            name: "FoodieUI"),
         .testTarget(
             name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            dependencies: ["FoodieUI"]),
     ]
 )
---
+++ Foodie/Sources/FoodieUI/DashboardView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+public struct DashboardView: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello World")
+    }
+}
